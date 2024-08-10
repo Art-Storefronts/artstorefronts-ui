65bb8b0439b535f93d4ba51e798d73ccbf7d07c3
Ty Barho <t.barho@gmail.com>
2024-08-10 16:53:42 -0500

    menubar, nav, pagination, popover, progress

---
+++ src/stories/pagination.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryFn } from '@storybook/react';
+import * as React from 'react';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+export default {
+  title: 'Components/Pagination',
+  component: Pagination,
+  subcomponents: {
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  },
+} as Meta;
+
+const Template: StoryFn = (args) => (
+  <Pagination {...args}>
+    <PaginationContent>
+      <PaginationItem>
+        <PaginationPrevious href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">1</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#" isActive>
+          2
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">3</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationEllipsis />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationNext href="#" />
+      </PaginationItem>
+    </PaginationContent>
+  </Pagination>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
