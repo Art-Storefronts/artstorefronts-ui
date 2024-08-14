86f038422e0ab092b4b39c9bc79c26fd725d024b
Matt Mills <mattmillsxyz@gmail.com>
2024-08-14 16:08:06 -0500

    Exclude stories from build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.tsx'],
+  entry: ['src/index.tsx', '!src/stores/**'],
   dts: true,
   clean: true,
   sourcemap: true,
