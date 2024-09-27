434d7e4b810b406693c0454d3b2911b31d6fa74f
Matt Mills <mattmillsxyz@gmail.com>
2024-09-27 17:51:10 -0500

    feat: adding close button

---
+++ src/components/ui/close-button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { XIcon } from "lucide-react";
+
+export interface CloseButtonProps {
+  size: "sm" | "md" | "lg";
+  onClick?: () => void;
+}
+
+const CloseButton: React.FC<CloseButtonProps> = ({ size, onClick }) => {
+  const getSize = (size: CloseButtonProps["size"]) => {
+    switch (size) {
+      case "sm":
+        return 16;
+      case "md":
+        return 24;
+      case "lg":
+        return 40;
+      default:
+        return 24;
+    }
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      onClick={onClick}
+      className={`p-0 rounded-full w-auto h-auto`}
+    >
+      <XIcon size={`${getSize(size)}px`} color="black" className="m-1" />
+    </Button>
+  );
+};
+
+export { CloseButton };
---
+++ src/stories/close-button.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { CloseButton, CloseButtonProps } from "@/components/ui/close-button";
+
+export default {
+  title: "Components/CloseButton",
+  component: CloseButton,
+  argTypes: {
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+    onClick: { action: "clicked" },
+  },
+  tags: ["autodocs"],
+} as Meta;
+
+const Template: StoryFn<CloseButtonProps> = (args) => <CloseButton {...args} />;
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "sm",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: "md",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "lg",
+};
