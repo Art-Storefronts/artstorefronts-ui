09bb306b32bd4e24d73844dd34de8936b8cc7ced
Matt Mills <mattmillsxyz@gmail.com>
2024-10-16 17:13:22 -0500

    chore: update margin on feature button

--- src/components/ui/feature-button.tsx
+++ src/components/ui/feature-button.tsx
@@ -22,7 +22,7 @@ const FeatureButton: React.FC<FeatureButtonProps> = ({
       onClick={onClick}
       className={`w-full flex items-center justify-between rounded-full py-7 px-4 ${className || ""}`}
     >
-      <div className="w-6 ml-2">{icon && icon}</div>
+      <div className="w-6 mx-2">{icon && icon}</div>
       <span className="text-lg font-bold">{text}</span>
       <div className="w-6 h-6 mr-2"></div>
     </Button>
