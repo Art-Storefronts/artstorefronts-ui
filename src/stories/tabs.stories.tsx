52b4004cb0b48cd73153b0254a7328dd0e4a8624
Matt Mills <mattmillsxyz@gmail.com>
2024-12-23 21:31:24 -0600

    chore: update tab styles

--- src/stories/tabs.stories.tsx
+++ src/stories/tabs.stories.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Meta, StoryFn } from '@storybook/react';
-import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { Meta, StoryFn } from "@storybook/react";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 type StoryProps = {
   defaultValue: string;
 };
 
 export default {
-  title: 'Components/Tabs',
+  title: "Components/Tabs",
   component: Tabs,
   subcomponents: { TabsList, TabsTrigger, TabsContent },
   args: {
-    defaultValue: 'one',
+    defaultValue: "one",
   },
   argTypes: {
     defaultValue: {
-      control: 'text',
+      control: "text",
     },
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 } as Meta;
 
 function Template(args: { defaultValue: string }) {
@@ -36,34 +35,22 @@ function Template(args: { defaultValue: string }) {
   );
 }
 
-function DisabledTemplate(args: { defaultValue: string }) {
-  return (
-    <Tabs {...args}>
-      <TabsList>
-        <TabsTrigger value="one">One</TabsTrigger>
-        <TabsTrigger value="two" disabled>
-          Two
-        </TabsTrigger>
-        <TabsTrigger value="three">Three</TabsTrigger>
-      </TabsList>
-      <TabsContent value="one">Content for tab one.</TabsContent>
-      <TabsContent value="two">Content for tab two.</TabsContent>
-      <TabsContent value="three">Content for tab three.</TabsContent>
-    </Tabs>
-  );
-}
-
 export const Default: StoryFn<StoryProps> = Template.bind({});
 Default.args = {
-  defaultValue: 'one',
+  defaultValue: "one",
 };
 
 export const WithDifferentDefault: StoryFn<StoryProps> = Template.bind({});
 WithDifferentDefault.args = {
-  defaultValue: 'two',
+  defaultValue: "two",
 };
 
-export const WithDisabledTab: StoryFn<StoryProps> = DisabledTemplate.bind({});
+export const WithDisabledTab: StoryFn<StoryProps> = Template.bind({});
 WithDisabledTab.args = {
-  defaultValue: 'one',
+  defaultValue: "one",
+};
+
+export const CustomVariant: StoryFn<StoryProps> = Template.bind({});
+CustomVariant.args = {
+  defaultValue: "one",
 };
