2810fa0bb564a4f0b690b83933a26fb7f57da107
Matt Mills <mattmillsxyz@gmail.com>
2026-02-01 15:57:19 -0600

    chore: make react types peer deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.tsx', '!src/stores/**'],
-  dts: {
-    resolve: true,
-  },
+  dts: true,
   clean: true,
   sourcemap: true,
   format: ['esm'],
   target: 'esnext',
   minify: true,
+  external: [
+    'react',
+    'react-dom',
+    'framer-motion',
+  ],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
 });
